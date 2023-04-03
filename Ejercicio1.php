53093ff7f63a7f17b174d0a2407bb23167e9886c
juanpaul29 <128315550+juanpaul29@users.noreply.github.com>
2023-04-03 13:50:36 -0400

    Se avanzo en clases la introducción a los vectores en php

---
+++ Ejercicio1.php
@@ -0,0 +1,20 @@
+<?php
+    $nombre = ["EVER",
+    "LUZ",
+    "MARTIN",
+    "JUAN PAUL",
+    "RODRIGO",
+    "JUAN RONALDO",
+    "CLAUDIA MARISOL",
+    "BORIS",
+    "YOLA",
+    "ROSSE MARY"];
+    //Muestra elemento del vector
+    //echo $nombre[6]."<br>";
+    //Muestra la posicion del elemento del vector
+    //$clave = array_search("CLAUDIA MARISOL",$nombre);
+    //echo $clave;
+
+    // Contar la cantidad de elemento del vector
+    echo "<br>La cantidad de elementos es:" .count ($nombre);
+?>
